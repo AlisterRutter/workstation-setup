5ac4a5b220f68377665e5518020f9f035b801a3b
David Meister <thedavidmeister@gmail.com>
2015-06-29 15:53:03 +1000

    mkdir before copy

--- install-sublime.sh
+++ install-sublime.sh
@@ -1,3 +1,4 @@
 brew tap caskroom/versions;
 brew cask install sublime-text3;
+mkdir ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User;
 cp ./Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings;
