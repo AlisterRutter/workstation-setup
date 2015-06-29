4f02b9de1aa762dd538126a7e9d429ef7097c13d
davidmeister <thedavidmeister@gmail.com>
2015-06-29 10:57:10 +1000

    install portal

---
+++ install-portal.sh
@@ -0,0 +1,11 @@
+brew cask install virtualbox vagrant;
+
+vagrant plugin install vagrant-cachier vagrant-hostmanager;
+
+brew install git git-flow node homebrew/php/php55;
+
+sudo gem install bundler;
+
+sudo gem install rake:10.4.2;
+
+git clone git@github.com:Equiem/THEPORTAL.git $HOME/theportal;
