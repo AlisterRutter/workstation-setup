a271d5259ce074d4b310d10a7b1ddc797cf19146
Matthew Scharley <matthew.scharley@equiem.com.au>
2015-08-03 16:11:31 +1000

    Fix creation of sublime preferences folder.

--- install-sublime.sh
+++ install-sublime.sh
@@ -2,5 +2,5 @@
 
 brew tap caskroom/versions;
 brew cask install --appdir=/Applications sublime-text3;
-mkdir ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User;
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User;
 cp ./Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings;
