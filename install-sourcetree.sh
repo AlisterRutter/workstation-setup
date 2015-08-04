ae14003418b6eaffec57612912c882d97b4660b9
Matthew Scharley <matthewscharley@Administrators-MacBook-Pro.local>
2015-08-04 14:21:21 +1000

    [#1] Set sourcetree to use the system installation of git from homebrew

--- install-sourcetree.sh
+++ install-sourcetree.sh
@@ -2,4 +2,12 @@
 
 brew cask install --appdir=/Applications sourcetree;
 
+if [ -e /usr/local/bin/git ]; then
+  # Use the system git instead of the built-in version. This also prevents issues
+  # with SourceTree mangling $PATH which prevents some git hooks from running
+  # correctly.
+  defaults write com.torusknot.SourceTreeNotMAS gitCustomPath '/usr/local/bin/git'
+  defaults write com.torusknot.SourceTreeNotMAS gitWhichOne -int 1
+fi
+
 brew cask install --appdir=/Applications p4merge;
