0a0f56b6521d7d95f1f233b1790baf0b1698f2f1
Jeremy Kirkham <jeremykirkham@192168015108.tpgi.com.au>
2015-08-21 14:03:17 +1000

    added bittorrent-sync and removed java from install

--- install-portal.sh
+++ install-portal.sh
@@ -2,12 +2,12 @@
 
 brew cask install --appdir=/Applications virtualbox vagrant;
 
+brew cask install --appdir=/Applications bittorrent-sync;
+
 vagrant plugin install vagrant-cachier vagrant-hostmanager;
 
 brew install git git-flow node homebrew/php/php55 homebrew/php/php55-mcrypt;
 sudo npm install -g bless
-
-brew cask install --appdir=/Applications java;
 
 . install-ruby.sh;
 
