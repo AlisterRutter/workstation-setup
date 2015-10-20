139cce84ff770d7cb5d466fcefede58d87f4c8d1
Amelia Schmidt <meelijane@users.noreply.github.com>
2015-10-20 11:18:03 +1100

    Update install-portal.sh

--- install-portal.sh
+++ install-portal.sh
@@ -14,3 +14,7 @@ sudo npm install -g bless
 gem install bundler:1.10.5 rake:10.4.2;
 
 git clone git@github.com:Equiem/THEPORTAL.git $HOME/theportal;
+
+sudo mkdir /Equiem;
+
+sudo chown `whoami`:admin /Equiem;
