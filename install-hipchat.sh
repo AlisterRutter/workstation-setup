beaf26231cf329c7fd12c2b0876579fb5d621304
Matthew Scharley <matt.scharley@gmail.com>
2015-11-11 10:01:44 +1100

    Add FlowDock to installation scripts.

--- install-hipchat.sh
+++ install-hipchat.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 brew cask install --appdir=/Applications hipchat;
+brew cask install --appdir=/Applications flowdock;
 
 # Adium is good for random IRC chats.
 brew cask install --appdir=/Applications adium;
