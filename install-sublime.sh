f9fb86d67877e56b15465633241ee1171f6bc16d
David Meister <thedavidmeister@gmail.com>
2016-04-12 14:30:31 +1000

    Update install-sublime.sh

--- install-sublime.sh
+++ install-sublime.sh
@@ -4,3 +4,5 @@ brew tap caskroom/versions;
 brew cask install --appdir=/Applications sublime-text3;
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User;
 cp ./Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings;
+
+brew cask install --appdir=/Applications atom;
