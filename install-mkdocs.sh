bc8e9df1902da5dd5e487e3a98c6fa1a27f29390
David Meister <thedavidmeister@gmail.com>
2016-04-19 18:37:17 +1000

    making install-mkdocs.sh match our docs

--- install-mkdocs.sh
+++ install-mkdocs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 brew install python;
-pip install mkdocs;
+# http://devdocs.equiem.com.au/getting-started-devdocs/
+sudo easy_install pip;
+sudo pip install mkdocs;
 
 git clone git@github.com:Equiem/devdocs.git ~/devdocs;
