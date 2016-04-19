9126e39610b22a126a85a46dd2ccd3d4fb4e00f1
Matthew Scharley <matt.scharley@gmail.com>
2016-04-19 14:52:45 +1000

    Sadly, vagrant still conflicts with bundler after all.

--- install-portal.sh
+++ install-portal.sh
@@ -9,7 +9,7 @@ sudo npm install -g bless@3.x
 
 . install-ruby.sh;
 
-gem install bundler rake:11.1.2;
+gem install bundler:1.10.6 rake:11.1.2;
 
 git clone git@github.com:Equiem/THEPORTAL.git $HOME/theportal;
 
