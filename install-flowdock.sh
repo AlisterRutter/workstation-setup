b4c0265fc48d71209ff862cb1f9f508a8c48a7ba
David Meister <thedavidmeister@gmail.com>
2016-06-03 14:17:53 +1000

    added slack

--- install-flowdock.sh
+++ install-flowdock.sh
@@ -4,3 +4,6 @@ brew cask install --appdir=/Applications flowdock;
 
 # Adium is good for random IRC chats.
 brew cask install --appdir=/Applications adium;
+
+# Many open source projects use Slack channels.
+brew cask install --appdir=/Applications slack;
